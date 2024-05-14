145e170bb004d5b364c12fb2f0ebb1288be15983
chucknguyen <chuckcamosun@gmail.com>
2024-05-14 01:03:49 -0700

    Add inventory fetching

--- routes/inventory.js
+++ routes/inventory.js
@@ -13,6 +13,17 @@ router.get('/', async (req,res) => {
         res.status(500).json({error: 'Internal server error'})
     }
 })
+
+router.get('/:id', async (req,res) => {
+    try {
+        const { id } = req.params;
+        const inventory = await myknex('inventory').where({id});
+        res.status(200).json(inventory);
+    } catch (error) {
+        console.error('Error fetching inventory', error)
+        res.status(500).json({error: 'Internal server error'})
+    }
+})
 router.post('/', async (req,res) => {
     try {
         const { user_id, item_name, quantity, price, description, media, brand, type, category } = req.body;
