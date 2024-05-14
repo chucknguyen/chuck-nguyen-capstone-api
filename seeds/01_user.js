a15f92e6d76a94f8399e0709bda1fddc20e147ed
chucknguyen <chuckcamosun@gmail.com>
2024-05-14 11:24:10 -0700

    Remove seed

--- seeds/01_user.js
+++ seeds/01_user.js
@@ -1,22 +1,4 @@
 export const seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('user').del();
-  await knex('user').insert([
-    {
-        username: 'john_doe',
-        password: 'password123',
-        email: 'john.doe@example.com',
-        name: 'John Doe',
-        created_at: new Date(),
-        updated_at: new Date()
-        },
-    {
-        username: 'jane_smith',
-        password: 'securepass',
-        email: 'jane.smith@example.com',
-        name: 'Jane Smith',
-        created_at: new Date(),
-        updated_at: new Date()
-        }
-  ]);
 };
--- seeds/02_inventory.js
+++ seeds/02_inventory.js
@@ -1,45 +1,4 @@
 export const seed = async function (knex) {
     // Deletes ALL existing entries
     await knex('inventory').del();
-    await knex('inventory').insert([
-        {
-            user_id: 1,
-            item_name: 'JFlower JF20-23',
-            quantity: 5,
-            price: 600,
-            description: 'High-quality pool cue with maple shaft and leather tip',
-            media: JSON.stringify(['http://localhost:8080/jflowers-1.jpg']),
-            brand: 'JFlower',
-            type: "Playing cue",
-            category: "Beginner",
-            created_at: new Date(),
-            updated_at: new Date()
-        },
-        {
-            user_id: 1, 
-            item_name: 'Predator P3 with Revo shaft',
-            quantity: 3,
-            price: 1500,
-            description: 'Professional-grade pool cue with carbon fiber shaft',
-            media: JSON.stringify(['http://localhost:8080/p3-revo.jpg']),
-            brand: 'Predator',
-            type: "Playing cue",
-            category: "Intermediate",
-            created_at: new Date(),
-            updated_at: new Date()
-        },
-        {
-            user_id: 2, 
-            item_name: 'SVB Ghost',
-            quantity: 2,
-            price: 800,
-            description: 'The best of Cuetec',
-            media: JSON.stringify(['http://localhost:8080/svb-ghost.jpg']),
-            brand: 'Cuetec',
-            type: "Playing cue",
-            category: "Intermediate",
-            created_at: new Date(),
-            updated_at: new Date()
-        }
-    ]);
   };
